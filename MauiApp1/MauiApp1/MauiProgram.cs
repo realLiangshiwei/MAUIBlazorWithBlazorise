79adc571d823b81d6f5b8494f7e917901830e7e8
liangshiwei <shiwei.liang@volosoft.com>
2022-11-10 20:36:28 +0800

    create maui app

---
+++ MauiApp1/MauiApp1/MauiProgram.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using MauiApp1.Data;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddBlazorise(options =>
+        {
+            options.Immediate = true;
+        })
+		.AddBootstrap5Providers()
+		.AddFontAwesomeIcons();
+
+        builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
